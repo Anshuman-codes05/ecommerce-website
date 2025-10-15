3c05f68b112f56aeea8e6ce843032147be6d698d
Anshuman <anshuman@example.com>
2025-10-15 22:19:33 +0530

    Fix wishlist button functionality - ensure buttons update correctly

--- public/js/script.js
+++ public/js/script.js
@@ -91,7 +91,7 @@ const outfitData = {
     2: {
         name: "Classic Elegance Ensemble",
         items: [
-            { id: '5', name: "Prada Buckle antiqued leather bag with belt", price: 8990.89, image: "https://www.prada.com/content/dam/pradabkg_products/2/2VG/2VG126/2HKFF077M/2VG126_2HKF_F077M_V_OJ2_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" },
+            { id: '5', name: "Buckle antiqued leather bag with belt", price: 8990.89, image: "https://www.prada.com/content/dam/pradabkg_products/2/2VG/2VG126/2HKFF077M/2VG126_2HKF_F077M_V_OJ2_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" },
             { id: '6', name: "Printed Fabric Boot", price: 1236.67, image: "https://www.prada.com/content/dam/pradabkg_products/2/2WE/2WE002/3LRAF0F24/2WE002_3LRA_F0F24_F_D001_SLR.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" },
             { id: '1', name: "Wool crew-neck sweater", price: 299.99, image: "https://www.prada.com/content/dam/pradabkg_products/U/UMF/UMF040/17KDF0036/UMF040_17KD_F0036_S_OOO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.1200.1200.jpg" },
             { id: '2', name: "Wool pants", price: 577.89, image: "https://www.prada.com/content/dam/pradabkg_products/U/UP0/UP0355/1W2GF0192/UP0355_1W2G_F0192_S_VMO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" }
@@ -103,7 +103,7 @@ const outfitData = {
             { id: '3', name: "Wool blouson jacket with shearling collar", price: 867.99, image: "https://www.prada.com/content/dam/pradabkg_products/S/SGC/SGC958/1UPKF0192/SGC958_1UPK_F0192_S_OOO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" },
             { id: '4', name: "Cotton pants", price: 499.77, image: "https://www.prada.com/content/dam/pradabkg_products/S/SPH/SPH485/1U7YF0012/SPH485_1U7Y_F0012_S_OOO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" },
             { id: '6', name: "Printed Fabric Boot", price: 1236.67, image: "https://www.prada.com/content/dam/pradabkg_products/2/2WE/2WE002/3LRAF0F24/2WE002_3LRA_F0F24_F_D001_SLR.jpg/_jcr_content/renditions/cq5dam.web.hebebed.1200.1200.jpg" },
-            { id: '5', name: "Prada Buckle antiqued leather bag with belt", price: 8990.89, image: "https://www.prada.com/content/dam/pradabkg_products/2/2VG/2VG126/2HKFF077M/2VG126_2HKF_F077M_V_OJ2_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" }
+            { id: '5', name: "Buckle antiqued leather bag with belt", price: 8990.89, image: "https://www.prada.com/content/dam/pradabkg_products/2/2VG/2VG126/2HKFF077M/2VG126_2HKF_F077M_V_OJ2_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" }
         ]
     },
     4: {
@@ -112,7 +112,7 @@ const outfitData = {
             { id: '1', name: "Wool crew-neck sweater", price: 299.99, image: "https://www.prada.com/content/dam/pradabkg_products/U/UMF/UMF040/17KDF0036/UMF040_17KD_F0036_S_OOO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.1200.1200.jpg" },
             { id: '3', name: "Wool blouson jacket with shearling collar", price: 867.99, image: "https://www.prada.com/content/dam/pradabkg_products/S/SGC/SGC958/1UPKF0192/SGC958_1UPK_F0192_S_OOO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" },
             { id: '2', name: "Wool pants", price: 577.89, image: "https://www.prada.com/content/dam/pradabkg_products/U/UP0/UP0355/1W2GF0192/UP0355_1W2G_F0192_S_VMO_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" },
-            { id: '5', name: "Prada Buckle antiqued leather bag with belt", price: 8990.89, image: "https://www.prada.com/content/dam/pradabkg_products/2/2VG/2VG126/2HKFF077M/2VG126_2HKF_F077M_V_OJ2_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" }
+            { id: '5', name: "Buckle antiqued leather bag with belt", price: 8990.89, image: "https://www.prada.com/content/dam/pradabkg_products/2/2VG/2VG126/2HKFF077M/2VG126_2HKF_F077M_V_OJ2_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg" }
         ]
     }
 };
@@ -277,7 +277,7 @@ const sampleMenProducts = [
      },
      {
          _id: '5',
-         name: "Prada Buckle antiqued leather bag with belt",
+         name: "Buckle antiqued leather bag with belt",
          description: "leather bag with printed logo and belt in beige, brown and remastered style of fashion.",
          price: 8990.89,
          image: "https://www.prada.com/content/dam/pradabkg_products/2/2VG/2VG126/2HKFF077M/2VG126_2HKF_F077M_V_OJ2_SLF.jpg/_jcr_content/renditions/cq5dam.web.hebebed.2400.2400.jpg",
